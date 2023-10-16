aea20108e666b58f881fe2091dfd31a4d61c63ed
abramovlad <147161193+abramovlad@users.noreply.github.com>
2023-10-16 22:22:37 +0300

    Add files via upload

---
+++ mpl1.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def lines():
+    x = np.array([-20, 20])
+    print('Формула графика y1:')
+    exec(input())
+    print('Формула графика y2:')
+    exec(input())
+    plt.title('Линейные функции:')
+    plt.xlabel('x')
+    plt.ylabel('y1, y2')
+    plt.grid()
+    plt.plot([x, x], [y1, y2])
+    plt.legend()
+    plt.show()
+
+
+lines()
+
+
+
