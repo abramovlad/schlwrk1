b8d7012584d03d6a9cec31600dbd8cc4a0b39fca
abramovlad <147161193+abramovlad@users.noreply.github.com>
2023-10-18 19:31:56 +0300

    Update mpl1.py

--- matplotlib/mpl1.py
+++ matplotlib/mpl1.py
@@ -1,20 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+x = np.array([-20, 20])
 
-def lines():
-    x = np.array([-20, 20])
+
+def lines(y1, y2):
     plt.title('Линейные функции:')
     plt.xlabel('x')
     plt.ylabel('y1, y2')
     plt.grid()
-    plt.plot(x, eval(input('Формула графика 1: y1 = ')))
-    plt.plot(x, eval(input('Формула графика 2: y2 = ')))
+    plt.plot(x, eval(y1))
+    plt.plot(x, eval(y2))
     plt.legend()
     plt.show()
 
 
-lines()
-
-
-
+lines(input('Формула графика 1: y1 = '), input('Формула графика 2: y2 = '))
